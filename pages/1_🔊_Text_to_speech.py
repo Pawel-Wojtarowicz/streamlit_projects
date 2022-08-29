1bc95c0c1a90254f68282cd38b4c20a8a28eed16
Pawel Wojtarowicz <wojtarowicz.pawel@gmail.com>
2022-08-29 12:15:06 +0200

    added the functionality to generate an audio file from a link or user input

--- pages/1_🔊_Text_to_speech.py
+++ pages/1_🔊_Text_to_speech.py
@@ -1,5 +1,55 @@
-from cgitb import small
-import streamlit as st 
+from newspaper import Article
+import nltk
+from gtts import gTTS
+import streamlit as st
 
-title = '<p style="color:White; font-size: 35px;">Text to speech</p>'
-st.markdown(title, unsafe_allow_html=True)
+
+def generate_mp3_file_from_link(data):
+    article = Article(data)
+    article.download()
+    article.parse()
+    nltk.download('punkt')
+    article.nlp()
+
+    mytext = article.text
+    language = 'pl'
+    myobj = gTTS(text=mytext, lang=language, slow=False)
+    myobj.save("read_article.mp3")
+
+    with open('read_article.mp3', 'rb') as f:
+        st.download_button('Download mp3 file', f, file_name='file.mp3')
+
+
+def generate_mp3_file_from_input(data):
+    mytext = data
+    language = 'pl'
+    myobj = gTTS(text=mytext, lang=language, slow=False)
+    myobj.save("read_article.mp3")
+    with open('read_article.mp3', 'rb') as f:
+        st.download_button('Download mp3 file', f, file_name='file.mp3')
+
+
+def main():
+    title = '<p style="color:White; font-size: 35px;">Convert text to speech</p>'
+    st.markdown(title, unsafe_allow_html=True)
+
+    with st.form(key="myform", clear_on_submit=True):
+        user_article = st.text_input("Enter your link")
+        user_article_text = st.text_area('or paste text to convert')
+        submit_button = st.form_submit_button("Generate mp3 file")
+
+        # TODO: add progress bar/status
+        # TODO: add file uploader maybe
+        # TODO: radio choice/language selectbox
+
+    if submit_button:
+        if user_article:
+            generate_mp3_file_from_link(user_article)
+        elif user_article_text:
+            generate_mp3_file_from_input(user_article_text)
+        else:
+            st.write("Error")
+
+
+if __name__ == '__main__':
+    main()
