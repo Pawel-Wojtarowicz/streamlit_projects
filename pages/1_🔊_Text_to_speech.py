6e278d174e8996c560c9f752b7cf16c6fc663261
Pawel Wojtarowicz <wojtarowicz.pawel@gmail.com>
2022-08-31 13:01:38 +0200

    added form logic and progress bar

--- pages/1_🔊_Text_to_speech.py
+++ pages/1_🔊_Text_to_speech.py
@@ -3,8 +3,10 @@ import nltk
 from gtts import gTTS
 import streamlit as st
 import os
+import time
 
 
+@st.cache(suppress_st_warning=True)
 def generate_mp3_file_from_link(data):
     article = Article(data)
     article.download()
@@ -15,19 +17,45 @@ def generate_mp3_file_from_link(data):
     mytext = article.text
     language = 'pl'
     myobj = gTTS(text=mytext, lang=language, slow=False)
-    myobj.save("read_article.mp3")
+    myobj.save("article.mp3")
 
-    with open('read_article.mp3', 'rb') as f:
+
+    #progress bar
+    my_bar = st.progress(0)
+    for percent_complete in range(100):
+        time.sleep(0.1)
+        my_bar.progress(percent_complete + 1)
+        
+    #downloading
+    with open('article.mp3', 'rb') as f:
         st.download_button('Download mp3 file', f, file_name='file.mp3')
 
 
+@st.cache(suppress_st_warning=True)
 def generate_mp3_file_from_input(data):
     mytext = data
     language = 'pl'
     myobj = gTTS(text=mytext, lang=language, slow=False)
-    myobj.save("read_article.mp3")
-    with open('read_article.mp3', 'rb') as f:
+    myobj.save("article.mp3")
+
+    #progress bar
+    my_bar = st.progress(0)
+    for percent_complete in range(100):
+        time.sleep(0.05)
+        my_bar.progress(percent_complete + 1)
+        
+    #downloading
+    with open('article.mp3', 'rb') as f:
         st.download_button('Download mp3 file', f, file_name='file.mp3')
+
+#form logic
+def check_form(user_article_link, user_article_input):
+    if user_article_link != "" and user_article_input != "":
+        st.error("Select only one form")
+    elif user_article_link != "":
+        generate_mp3_file_from_link(user_article_link)
+    else:
+        generate_mp3_file_from_input(user_article_input)
 
 
 def main():
@@ -41,24 +69,18 @@ def main():
     title = '<p style="color:White; font-size: 35px;">Convert text to speech</p>'
     st.markdown(title, unsafe_allow_html=True)
 
-    with st.form(key="myform", clear_on_submit=True):
-        user_article = st.text_input("Enter your link")
-        user_article_text = st.text_area('or paste text to convert')
-        submit_button = st.form_submit_button("Generate mp3 file")
+    with st.form(key="myform"):
+        user_article_link = st.text_input("Enter your link")
+        user_article_input = st.text_area('or paste text to convert')
+        submitted = st.form_submit_button("Generate mp3 file")
 
-        # TODO: add progress bar/status
-        # TODO: add file uploader maybe
-        # TODO: radio choice/language selectbox
-        # TODO: session state
-
-    if submit_button:
-        if user_article:
-            generate_mp3_file_from_link(user_article)
-        elif user_article_text:
-            generate_mp3_file_from_input(user_article_text)
-        else:
-            st.write("Error")
+    if submitted:
+        check_form(user_article_link, user_article_input)
 
 
 if __name__ == '__main__':
     main()
+
+
+# TODO: add file uploader maybe
+# TODO: radio choice/language selectbox
