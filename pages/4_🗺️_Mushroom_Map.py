dac658bed4dc8e11fe00ffae16f6825e45cf43f8
Pawel Wojtarowicz <wojtarowicz.pawel@gmail.com>
2022-09-16 12:10:34 +0200

    added deletion of data, improved linking of markers to map and displaying the list of user places

--- pages/4_🗺️_Mushroom_Map.py
+++ pages/4_🗺️_Mushroom_Map.py
@@ -2,46 +2,67 @@ import streamlit as st
 import pandas as pd
 import folium
 from streamlit_folium import st_folium
-
-
-st.title("Mushroom Map")
-if "mdf" not in st.session_state:
-    st.session_state.mdf = pd.DataFrame(
-        columns=["Localization", "Latitude", "Longitude"])
-
+import os
+from streamlit.runtime.legacy_caching import clear_cache as clear_cache
 
 # TODO: add possibility to load file with coordinates
-# TODO: add possibility to clear map
+# TODO: add possiblity to add markers without coordinates, just enter street name
+# TODO: add more details to markers
+
+st.title("Mushroom Map")
+st.sidebar.success("Select project above.")
+css_path = "styles\main.css"
+abs_path = os.path.abspath(css_path)
+
+
+with open(abs_path) as f:
+    st.markdown("<style>{}</style>".format(f.read()),
+                unsafe_allow_html=True)
+
+if "mdf" not in st.session_state:
+    st.session_state.mdf = pd.DataFrame(
+        columns=["Label", "Latitude", "Longitude"])
 
 map = folium.Map(location=[52.32, 19.42], zoom_start=6)
+
 col1, col2, col3 = st.columns(3)
+label = col1.text_input("Label")
+latitude = col2.number_input("Latitude", step=1., format="%.7f")
+longitude = col3.number_input("Longitude", step=1., format="%.7f")
 
-localization = col1.text_input("Localization")
-latitude = col2.number_input("Latitude", step=1., format="%.2f")
-longitude = col3.number_input("Longitude", step=1., format="%.2f")
-
-col1, col2 = st.columns([1, 18])
-
+col1, col2 = st.columns([1, 16])
 with col1:
     run = st.button("Submit", key="Submit")
 with col2:
     clear = st.button("Clear", key="Clear")
+    clear_cache()
 
-df_new = pd.DataFrame({"Localization": localization,
+df_new = pd.DataFrame({"Label": label,
                        "Latitude": latitude,
-                       "Longitude": longitude}, index=[localization])
+                       "Longitude": longitude}, index=[label])
 
 if run:
     st.session_state.mdf = pd.concat([st.session_state.mdf, df_new], axis=0)
 
 if clear:
-    st.write("df cleared")
+    st.session_state.mdf.drop(st.session_state.mdf.index, inplace=True)
 
-# st.dataframe(st.session_state.mdf)
 
-for index, row in st.session_state.mdf.iterrows():
-    folium.Marker(location=[row["Latitude"], row["Longitude"]], tooltip=[
-                  "Szerokość", row["Latitude"], "Długość", row["Longitude"]], popup=row["Localization"]).add_to(map)
+col1, col2, col3 = st.columns([1.1, 0.1, 3])
+with col1:
+    hide_table_row_index = """
+            <style>
+            thead tr th:first-child {display:none}
+            tbody th {display:none}
+            </style>
+            """
 
-st.data = st_folium(map, width=800, height=500)
-# st.write(f"Total Rows: {st.session_state.mdf.shape[0]}")
+    st.markdown(hide_table_row_index, unsafe_allow_html=True)
+    st.table(st.session_state.mdf)
+
+for i in range(0, len(st.session_state.mdf)):
+    folium.Marker(location=[st.session_state.mdf.iloc[i]["Latitude"], st.session_state.mdf.iloc[i]
+                  ["Longitude"]], popup=st.session_state.mdf.iloc[i]["Label"]).add_to(map)
+
+with col3:
+    st.data = st_folium(map, width=1000, height=500)
