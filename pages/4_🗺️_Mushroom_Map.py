708a2bc00473a6e9c2cc55f0ed14a3d04a1e6aa3
Pawel Wojtarowicz <wojtarowicz.pawel@gmail.com>
2022-09-21 13:48:31 +0200

    added possibility to pick localization from picking a point on the map, implemented lat/lng session.state, other small changes

--- pages/4_🗺️_Mushroom_Map.py
+++ pages/4_🗺️_Mushroom_Map.py
@@ -1,3 +1,4 @@
+from webbrowser import get
 import streamlit as st
 import pandas as pd
 import folium
@@ -10,6 +11,7 @@ from folium.plugins import Draw
 # TODO: add possibility to load file with coordinates
 # TODO: add possiblity to add markers without coordinates, just enter street name
 
+
 st.title("Mushroom Map")
 st.sidebar.success("Select project above.")
 css_path = "styles\main.css"
@@ -20,25 +22,29 @@ with open(abs_path) as f:
     st.markdown("<style>{}</style>".format(f.read()),
                 unsafe_allow_html=True)
 
+#------SESSION STATE
 if "mdf" not in st.session_state:
     st.session_state.mdf = pd.DataFrame(
         columns=["Label", "Latitude", "Longitude", "Quantity"])
+    
+if "lat" not in st.session_state:
+    st.session_state.lat = 52.09
+    
+if "lng" not in st.session_state:
+    st.session_state.lng = 19.59
 
-map = folium.Map(location=[52.32, 19.42], zoom_start=6)
-
+#------MAP
+map = folium.Map(location=[52.05, 19.59], zoom_start=6)
 my_marker = folium.LatLngPopup()
 map.add_child(my_marker)  
 
-#TODO: check another version of the map 
-# Draw(export=False).add_to(map)
-# output = st_folium(map, width=500, height=500)
-# st.write(output)
-  
+#------USER INPUTS
+#TODO: change to myform with on_change function   
 col1, col2, col3, col4 = st.columns(4)
 label = col1.text_input("Label")
-latitude = col2.number_input("Latitude", step=1., format="%.7f")
-longitude = col3.number_input("Longitude", step=1., format="%.7f")
-quantity = col4.number_input("Quantinty per hour", step=1)
+latitude = col2.number_input(label="Latitude", value=st.session_state.lat, step=1., format="%.7f")
+longitude = col3.number_input(label="Longitude", value=st.session_state.lng, step=1., format="%.7f")
+quantity = col4.number_input("Quantinty per hour", value=0, step=1)
 
 col1, col2 = st.columns([1, 16])
 with col1:
@@ -47,20 +53,25 @@ with col2:
     clear = st.button("Clear", key="Clear")
     clear_cache()
 
-df_new = pd.DataFrame({"Label": label,
-                       "Latitude": latitude,
-                       "Longitude": longitude,
-                       "Quantity": quantity}, index=[label])
 
+#------DATA FRAME WITH PARAMETERS
+df_new = pd.DataFrame({"Label": label,
+                    "Latitude": latitude,
+                    "Longitude": longitude,
+                    "Quantity": quantity}, index=[label])
+
+#------CONCATENATE DATA FRAMES
 if run:
     st.session_state.mdf = pd.concat([st.session_state.mdf, df_new], axis=0)
     st.session_state.mdf['marker_color'] = pd.cut(st.session_state.mdf['Quantity'], bins=4,
-                                                  labels=['orange', 'lightred', 'red', 'darkred'])
+                                                labels=['orange', 'lightred', 'red', 'darkred'])
 
+#------CLEARING
 if clear:
     st.session_state.mdf.drop(st.session_state.mdf.index, inplace=True)
 
 
+#------DISPAYING USER DATA
 col1, col2, col3 = st.columns([1.1, 0.1, 3])
 with col1:
     hide_table_row_index = """
@@ -72,15 +83,18 @@ with col1:
 
     st.markdown(hide_table_row_index, unsafe_allow_html=True)
     st.write("My data")
-    st.table(st.session_state.mdf)
+    st.table(st.session_state.mdf[["Label", "Latitude", "Longitude", "Quantity"]])
 
+#TODO: improve displaying tooltip
 for i in range(0, len(st.session_state.mdf)):
     folium.Marker(location=[st.session_state.mdf.iloc[i]["Latitude"], st.session_state.mdf.iloc[i]
                             ["Longitude"]], popup=st.session_state.mdf.iloc[i]["Label"], tooltip=st.session_state.mdf.iloc[i]["Quantity"], icon=folium.Icon(color=st.session_state.mdf.iloc[i]['marker_color'])).add_to(map)
 
 with col3:
-    st.data = st_folium(map, width=1000, height=500)
-   
-
-
-
+    # Draw(export=True).add_to(map)
+    output = st_folium(map, width=1000, height=500)
+    # st.write(output)
+    if (output["last_clicked"] != None ):
+        st.session_state.lat = output["last_clicked"]["lat"]
+        st.session_state.lng = output["last_clicked"]["lng"]
+        
