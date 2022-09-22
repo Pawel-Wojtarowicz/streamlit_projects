e0816b7585e9b63f2c0bee6e99a52b8c1e026e11
Pawel Wojtarowicz <wojtarowicz.pawel@gmail.com>
2022-09-22 12:50:46 +0200

    removed coordinates imputs, small changes

--- pages/4_🗺️_Mushroom_Map.py
+++ pages/4_🗺️_Mushroom_Map.py
@@ -1,3 +1,4 @@
+from turtle import onclick
 from webbrowser import get
 import streamlit as st
 import pandas as pd
@@ -7,20 +8,13 @@ import os
 from streamlit.runtime.legacy_caching import clear_cache as clear_cache
 from folium.plugins import Draw
 
-
-# TODO: add possibility to load file with coordinates
 # TODO: add possiblity to add markers without coordinates, just enter street name
-
 
 st.title("Mushroom Map")
 st.sidebar.success("Select project above.")
 css_path = "styles\main.css"
 abs_path = os.path.abspath(css_path)
 
-
-with open(abs_path) as f:
-    st.markdown("<style>{}</style>".format(f.read()),
-                unsafe_allow_html=True)
 
 #------SESSION STATE
 if "mdf" not in st.session_state:
@@ -32,32 +26,36 @@ if "lat" not in st.session_state:
     
 if "lng" not in st.session_state:
     st.session_state.lng = 19.59
+  
+with open(abs_path) as f:
+    st.markdown("<style>{}</style>".format(f.read()),
+                unsafe_allow_html=True)
 
 #------MAP
 map = folium.Map(location=[52.05, 19.59], zoom_start=6)
 my_marker = folium.LatLngPopup()
-map.add_child(my_marker)  
+map.add_child(my_marker)
 
 #------USER INPUTS
-#TODO: change to myform with on_change function   
-col1, col2, col3, col4 = st.columns(4)
-label = col1.text_input("Label")
-latitude = col2.number_input(label="Latitude", value=st.session_state.lat, step=1., format="%.7f")
-longitude = col3.number_input(label="Longitude", value=st.session_state.lng, step=1., format="%.7f")
-quantity = col4.number_input("Quantinty per hour", value=0, step=1)
-
-col1, col2 = st.columns([1, 16])
+#TODO: move buttons to side-by-side
+col1, col2 = st.columns([3,10])
 with col1:
+    label = st.text_input("Label")
+    quantity = st.number_input("Quantinty per hour", value=0, step=1)
     run = st.button("Submit", key="Submit")
-with col2:
     clear = st.button("Clear", key="Clear")
-    clear_cache()
+              
+with col2:
+    # Draw(export=True).add_to(map)
+    output = st_folium(map, width=1000, height=500)
+    if (output["last_clicked"] != None ):
+        st.session_state.lat = output["last_clicked"]["lat"]
+        st.session_state.lng = output["last_clicked"]["lng"]
 
-
-#------DATA FRAME WITH PARAMETERS
+#------CREATING NEW DATA FRAME WITH USER PARAMETERS
 df_new = pd.DataFrame({"Label": label,
-                    "Latitude": latitude,
-                    "Longitude": longitude,
+                    "Latitude": st.session_state.lat,
+                    "Longitude": st.session_state.lng,
                     "Quantity": quantity}, index=[label])
 
 #------CONCATENATE DATA FRAMES
@@ -66,10 +64,10 @@ if run:
     st.session_state.mdf['marker_color'] = pd.cut(st.session_state.mdf['Quantity'], bins=4,
                                                 labels=['orange', 'lightred', 'red', 'darkred'])
 
-#------CLEARING
+#------CLEARING DATA
 if clear:
     st.session_state.mdf.drop(st.session_state.mdf.index, inplace=True)
-
+    clear_cache()
 
 #------DISPAYING USER DATA
 col1, col2, col3 = st.columns([1.1, 0.1, 3])
@@ -90,11 +88,4 @@ for i in range(0, len(st.session_state.mdf)):
     folium.Marker(location=[st.session_state.mdf.iloc[i]["Latitude"], st.session_state.mdf.iloc[i]
                             ["Longitude"]], popup=st.session_state.mdf.iloc[i]["Label"], tooltip=st.session_state.mdf.iloc[i]["Quantity"], icon=folium.Icon(color=st.session_state.mdf.iloc[i]['marker_color'])).add_to(map)
 
-with col3:
-    # Draw(export=True).add_to(map)
-    output = st_folium(map, width=1000, height=500)
-    # st.write(output)
-    if (output["last_clicked"] != None ):
-        st.session_state.lat = output["last_clicked"]["lat"]
-        st.session_state.lng = output["last_clicked"]["lng"]
         
