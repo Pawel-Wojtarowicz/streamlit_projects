1c3ef557d3cde72babf46a95d642309142a61624
Pawel Wojtarowicz <wojtarowicz.pawel@gmail.com>
2022-10-03 11:20:54 +0200

    minor changes

--- pages/4_🗺️_Mushroom_Map.py
+++ pages/4_🗺️_Mushroom_Map.py
@@ -6,10 +6,8 @@ from streamlit_folium import st_folium
 import os
 
 
-# TODO: add possiblity to add markers without coordinates, just enter street name
 title = '<p style="color:White; font-size: 35px;">Mushroom Map</p>'
 st.markdown(title, unsafe_allow_html=True)
-st.write("Please pick a point on the map")
 st.sidebar.success("Select project above.")
 css_path = "styles\main.css"
 abs_path = os.path.abspath(css_path)
@@ -17,7 +15,7 @@ abs_path = os.path.abspath(css_path)
 # ------SESSION STATE
 if "mdf" not in st.session_state:
     st.session_state.mdf = pd.DataFrame(
-        columns=["Label", "Latitude", "Longitude", "Quantity"])
+        columns=["Description", "Latitude", "Longitude", "Quantity"])
 
 if "lat" not in st.session_state:
     st.session_state.lat = 52.09
@@ -25,63 +23,58 @@ if "lat" not in st.session_state:
 if "lng" not in st.session_state:
     st.session_state.lng = 19.59
 
-
 with open(abs_path) as f:
     st.markdown("<style>{}</style>".format(f.read()),
                 unsafe_allow_html=True)
 
 # ------MAP
-
-map = folium.Map(location=[52.05, 19.59], zoom_start=6)
-my_marker = folium.LatLngPopup()
+map = folium.Map(location=[st.session_state.lat,
+                 st.session_state.lng], zoom_start=6)
+my_marker = folium.ClickForMarker()
 map.add_child(my_marker)
 
 
 # ------USER INPUTS
 col1, col2, col3 = st.columns([2, 0.6, 0.5])
-
-label = col1.text_input(label="Description")
-quantity = col2.number_input(label="Quantity per Hour", value=0, step=1)
+description = col1.text_input(label="Enter description")
+quantity = col2.number_input(
+    label="Quantity per Hour", value=0, min_value=0, step=1)
 with col3:
     run = st.button(label="Submit", key="Submit")
     clear = st.button(label="Clear", key="Clear")
 
-
 # ------CREATING NEW DATA FRAME WITH USER PARAMETERS
-df_new = pd.DataFrame({"Label": label,
+df_new = pd.DataFrame({"Description": description,
                        "Latitude": st.session_state.lat,
                        "Longitude": st.session_state.lng,
-                       "Quantity": quantity}, index=[label])
+                       "Quantity": quantity}, index=[description])
 
 # ------CONCATENATE DATA FRAMES
-#TODO: add validation
 if run:
     st.session_state.mdf = pd.concat([st.session_state.mdf, df_new], axis=0)
     st.session_state.mdf['marker_color'] = pd.cut(st.session_state.mdf['Quantity'], bins=4,
-                                                labels=['orange', 'lightred', 'red', 'darkred'])
+                                                  labels=['orange', 'lightred', 'red', 'darkred'])
 
 # ------CLEARING DATA
 if clear:
     st.session_state.mdf.drop(st.session_state.mdf.index, inplace=True)
     clear_cache()
 
-# ------DISPAYING USER DATA
-col1, col2 = st.columns([3, 1.5])
-with col1:
-    st.table(
-        st.session_state.mdf[["Label", "Latitude", "Longitude", "Quantity"]])
-
-
 for i in range(0, len(st.session_state.mdf)):
     folium.Marker(location=[st.session_state.mdf.iloc[i]["Latitude"], st.session_state.mdf.iloc[i]
-                            ["Longitude"]], popup=st.session_state.mdf.iloc[i]["Label"], tooltip=st.session_state.mdf.iloc[i]["Quantity"], icon=folium.Icon(color=st.session_state.mdf.iloc[i]['marker_color'])).add_to(map)
+                            ["Longitude"]], popup=st.session_state.mdf.iloc[i]["Description"], tooltip=st.session_state.mdf.iloc[i]["Quantity"], icon=folium.Icon(color=st.session_state.mdf.iloc[i]['marker_color'])).add_to(map)
 
-output = st_folium(map, width=880, height=500)     
+st.write("Please pick a point on the map")
+output = st_folium(map, width=880, height=500)
 if (output["last_clicked"] != None):
     st.session_state.lat = output["last_clicked"]["lat"]
     st.session_state.lng = output["last_clicked"]["lng"]
 
+# ------DISPAYING USER DATA
+col1, col2 = st.columns([3, 1.5])
+with col1:
+    st.write("Added places")
+    st.table(
+        st.session_state.mdf[["Description", "Latitude", "Longitude", "Quantity"]])
 
-#TODO: add export map to png
-
-
+# TODO: add export map to png
--- pages/5_🧩_Other_projects.py
+++ pages/5_🧩_Other_projects.py
@@ -13,16 +13,24 @@ def main():
         st.markdown("<style>{}</style>".format(f.read()),
                     unsafe_allow_html=True)
 
-    PROJECTS = {"🔗 Streamlit Twitter - download the user's tweets and ploting a chart": "https://twitter--streamlit.herokuapp.com",
-                "🔗 Twitter movie notification - tells you if a movie is on TV and sends a tweet": "https://twitter.com/czy_kiler",
-                "🔗 GUS Dashboard app - historical and forecast yearbook data from the Central Statistical Office": "https://pandas-streamlit.herokuapp.com",
-                "🔗 Lotto - is it worth playing or not": "https://github.com/Pawel-Wojtarowicz/Python/tree/master/Lotto",
-                "🔗 Password manager - GUI password manager written with tkinter": "https://github.com/Pawel-Wojtarowicz/password_manager",
+    PROJECTS = {"🔗 Streamlit Twitter - download the user's tweets and plot a chart": ["https://twitter--streamlit.herokuapp.com",
+                """- In this project, I learned how to work with the Twitter API and the Twitter Scrapper, as well
+                     as how to save and load data to/from files such as CSV and how to plot simple charts."""],
+                "🔗 Twitter movie notification - tells you if a movie is on TV and sends a tweet": ["https://github.com/Pawel-Wojtarowicz/Python/tree/master/twitter", 
+                "- I have learned how to scrapp data from web page without API"],
+                "🔗 GUS Dashboard app - historical and forecast yearbook data from the Central Statistical Office": ["https://pandas-streamlit.herokuapp.com", 
+                "- I learned about the Pandas library and how to create simple dataframes and operate on the data."],
+                "🔗 Password manager - GUI password manager written with tkinter": ["https://github.com/Pawel-Wojtarowicz/password_manager",
+                """- In this project, I learned about the tkinter library and learned how to build a GUI application. 
+                I learned the basics of cryptography to create the secret key needed to encrypt provided user passwords."""],
+                "🔗 Lotto - is it worth playing or not": ["https://github.com/Pawel-Wojtarowicz/Python/tree/master/Lotto", ""],
                 }
 
     st.write("---")
-    for project, link in PROJECTS.items():
-        st.write(f"[{project}]({link})")
+    for project, data in PROJECTS.items():
+        st.subheader(f"[{project}]({data[0]})")
+        st.write(f"{data[1]}")
+        st.write("---")
 
 
 if __name__ == "__main__":
